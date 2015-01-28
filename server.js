18b5441363556833137d30c568c4e157e8c2200c
Sergio <serg.guillen@gmail.com>
2015-01-28 05:59:54 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send(' Hello MGT-656!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('Dull-Fish');
 });
 
 app.listen(process.env.PORT || 4000);
